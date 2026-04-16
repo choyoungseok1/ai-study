3769e4c0d9050e594ada1753cbbd82e1b74786fd
choyoungseok1 <cys3409@gmail.com>
2026-04-16 20:12:51 +0900

    2026-04-16 : 오늘 학습한 내용 한 줄

---
+++ quick-push.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$Message
+)
+
+$date = Get-Date -Format "yyyy-MM-dd"
+git add .
+git commit -m "$date : $Message"
+git push
+Write-Host "✅ Pushed: $date : $Message" -ForegroundColor Green
